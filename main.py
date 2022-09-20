10b9f3669478322aac716957dcc2a1395637cc4a
Gregorio Queiroz <gregorio.queiroz@ipeas.local>
2022-09-20 09:15:25 -0300

    # O algorimto mais sofisticado resolve 98% dos casos [0-100] porém deixei implementado a solução que abraça alguns dos casos com expressão matemática (x operador y) operador (w operador z)

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ def generate_expression(n):
     return express
 
 def gerar_expressao(n):
+    # esse algorimo resolve 98% dos casos [0-100] porém deixei implementado a solução que abraça alguns dos casos.
     if n in [3,4,5,6,10,11,12,13,14,18,19,20,21,22,23,25,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41,42,46,47,48,49,50,51,53,54,55,56,57,58,59,60,61,62,63,65,66,67,69,68,70,71,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,89,90,92,94,95,96,97,98,100]:
         return gerar_expressao2(n)    
     elif n in [83,91,93]:
